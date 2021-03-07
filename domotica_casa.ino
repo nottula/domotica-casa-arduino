c1d25531836cf3b86bcbbe07cb6ba1d0a759be84
Marco <marco.fioretti@maticmind.it>
2021-03-07 22:52:49 +0100

    codice pulito e aggiunta camera e bagno notte

--- domotica_casa.ino
+++ domotica_casa.ino
@@ -1,4 +1,3 @@
-
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
@@ -6,16 +5,15 @@
 #include <Ethernet.h>
 #include <PubSubClient.h>
 
-
 //DEFINIZIONE ZONE E PIN
-#define PINCAMERA 46 
+#define PINCAMERA 45
+#define PINBAGNONOTTE 46 
 #define PINBAGNO 47
 #define PINPRANZO 48 
 #define PINSALONE 49 
 #define PINCUCINA 50 
 #define PINESTERNA 51 
 #define PINSCALE 52 
-//#define PIN_NON_UTILIZZATO 53 
 
 //DEFINIZIONE TIPOLOGIA SENSORE
 #define DHTTYPE    DHT11     // DHT 11
@@ -23,8 +21,7 @@
 // Update these with values suitable for your network.
 byte mac[]= { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 100);
-IPAddress server(192, 168, 1, 11);
-
+IPAddress server(192, 168, 1, 12);
 
 
 
@@ -33,9 +30,8 @@ EthernetClient ethClient;
 PubSubClient client(ethClient);
 /////////////////////////////////////////////////
 
-
-
 DHT_Unified dhtcamera(PINCAMERA, DHTTYPE);
+DHT_Unified dhtnottebagno(PINBAGNONOTTE, DHTTYPE);
 DHT_Unified dhtbagno(PINBAGNO, DHTTYPE);
 DHT_Unified dhtpranzo(PINPRANZO, DHTTYPE);
 DHT_Unified dhtsalone(PINSALONE, DHTTYPE);
@@ -44,14 +40,12 @@ DHT_Unified dhtesterna(PINESTERNA, DHTTYPE);
 DHT_Unified dhtscale(PINSCALE, DHTTYPE);
 //DHT_Unified dhtc(PIN_NON_UTILIZZATO, DHTTYPE);
 
-uint32_t delayMS;
-
-
-
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
   dhtcamera.begin();
   dhtbagno.begin();
+  dhtcamera.begin();
+  dhtnottebagno.begin();
   sensor_t sensor;
   client.setServer(server, 1883);
   client.setCallback(callback);
@@ -61,7 +55,7 @@ void setup() {
   delay(1500);
 
 }
-
+uint32_t delayMS;
 
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -95,96 +89,69 @@ void reconnect() {
   }
 }
 
-
 void loop() {
   String valore ="";
   char valore_send[5];
-  
-  // Delay between measurements.
+
   delay(1000);
-  // Get temperature event and print its value.
+ 
   sensors_event_t event;
-  dhtcamera.temperature().getEvent(&event);
-  Serial.println("******* CAMERA ********* ");
-  Serial.println(event.temperature);
-  
-  dhtcamera.humidity().getEvent(&event);
-  Serial.println(event.relative_humidity);
-  Serial.println("--------------------------");
 
 
-  
-
-  dhtbagno.temperature().getEvent(&event);
-  Serial.println("******* BAGNO ********* ");
-  Serial.println(event.temperature);
-   if (!client.connected()) {
+  if (!client.connected()) {
     reconnect();
   }
   client.loop();
-  client.subscribe("temp_bagno");
-    
-    //float tempc1;
-   // var char buffero;
-    
-   // String temperatura (floatToString(buffero, tempc1, 1));
+//////////////////////////////////////////////////////
+//////////////////BAGNO GIORNO
+/////////////////////////////////////////////////////
+  dhtbagno.temperature().getEvent(&event);
+  client.subscribe("temp_bagno");   
    valore=String(event.temperature)+"°C";
    Serial.println(valore);
    valore.toCharArray(valore_send,5);
-   if(client.publish("temp_bagno", valore_send))
-        
-  {
-    Serial.println("Publish message success");
-    Serial.print(valore);
-  }
-  else
-  {
-    Serial.println("Could not send message :(");
-  }
-
- 
-  // Dont overload the server!
-  delay(4000);
-
-  
-
-  
+   client.publish("temp_bagno", valore_send);
+          
   dhtbagno.humidity().getEvent(&event);
-  Serial.println(event.relative_humidity);
-  Serial.println("--------------------------");
-
-  client.subscribe("um_bagno");
-    
-    //float tempc1;
-   // var char buffero;
-    
-   // String temperatura (floatToString(buffero, tempc1, 1));
+  client.subscribe("um_bagno");    
    valore=String(event.relative_humidity);
    Serial.println(valore);
    valore.toCharArray(valore_send,5);
-   if(client.publish("um_bagno", valore_send))
+   client.publish("um_bagno", valore_send);
+//////////////////////////////////////////////////////
+//////////////////BAGNO NOTTE
+/////////////////////////////////////////////////////
+  dhtnottebagno.temperature().getEvent(&event);
+  client.subscribe("notte_temp_bagno");   
+   valore=String(event.temperature)+"°C";
+   Serial.println(valore);
+   valore.toCharArray(valore_send,5);
+   client.publish("notte_temp_bagno", valore_send);
+          
+  dhtnottebagno.humidity().getEvent(&event);
+  client.subscribe("notte_um_bagno");    
+   valore=String(event.relative_humidity);
+   Serial.println(valore);
+   valore.toCharArray(valore_send,5);
+   client.publish("notte_um_bagno", valore_send);
+   //////////////////////////////////////////////////////
+//////////////////CAMERA
+/////////////////////////////////////////////////////
+  dhtcamera.temperature().getEvent(&event);
+  client.subscribe("temp_camera");   
+   valore=String(event.temperature)+"°C";
+   Serial.println(valore);
+   valore.toCharArray(valore_send,5);
+   client.publish("temp_camera", valore_send);
+          
+  dhtcamera.humidity().getEvent(&event);
+  client.subscribe("notte_um_bagno");    
+   valore=String(event.relative_humidity);
+   Serial.println(valore);
+   valore.toCharArray(valore_send,5);
+   client.publish("um_camera", valore_send);
         
-  {
-    Serial.println("Publish message success");
-    Serial.print(valore);
-  }
-  else
-  {
-    Serial.println("Could not send message :(");
-  }
+  
  client.disconnect();
 
-
-
-
-
-
-
-
-
-
-
-
-
- }
-  
+}
