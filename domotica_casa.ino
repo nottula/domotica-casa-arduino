ae940dcfcc1dc637251d93a0a64c50b4159c1a7c
Marco <marco.fioretti@maticmind.it>
2021-03-11 08:37:55 +0100

    inserito sensore luce e risposte mqtt

--- domotica_casa.ino
+++ domotica_casa.ino
@@ -5,6 +5,12 @@
 #include <Ethernet.h>
 #include <PubSubClient.h>
 
+//DEFINIZIONE VARIABILI LUMINOSITA'
+int lux_camera = 0;
+
+//DEFINIZIONE VARIABILI AUTO MODE LUX
+int autolux_camera=0;
+
 //DEFINIZIONE ZONE E PIN
 #define PINCAMERA 45
 #define PINBAGNONOTTE 46 
@@ -14,6 +20,13 @@
 #define PINCUCINA 50 
 #define PINESTERNA 51 
 #define PINSCALE 52 
+
+
+//DEFINIZIONE RITORNI CONTROLLO LUCI
+int PINRITORNOCAMERA = 22;
+
+
+//DEFINIZIONE ZONE LUCE
 
 //DEFINIZIONE TIPOLOGIA SENSORE
 #define DHTTYPE    DHT11     // DHT 11
@@ -41,12 +54,19 @@ DHT_Unified dhtscale(PINSCALE, DHTTYPE);
 //DHT_Unified dhtc(PIN_NON_UTILIZZATO, DHTTYPE);
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
+
+  //INIZIALIZZAZIONE DHT11
   dhtcamera.begin();
   dhtbagno.begin();
   dhtcamera.begin();
   dhtnottebagno.begin();
   sensor_t sensor;
+
+  //INIZIALIZZAZIONE RITORNI LUCE
+  pinMode(PINRITORNOCAMERA, INPUT);
+
+  //INIZIALIZZAZIONE MQTT CLIENT
   client.setServer(server, 1883);
   client.setCallback(callback);
 
@@ -62,9 +82,16 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i=0;i<length;i++) {
-    Serial.print((char)payload[i]);
+
+  if(topic ="luce_camera"){
+    String str_lux=""; 
+      for (int i=0;i<length;i++) {
+        str_lux=str_lux+(char)payload[i];
+      }
+    lux_camera=str_lux.toInt();      
   }
+  
+  
   Serial.println();
 }
 
@@ -93,7 +120,7 @@ void loop() {
   String valore ="";
   char valore_send[5];
 
-  delay(1000);
+  delay(100);
  
   sensors_event_t event;
 
@@ -105,53 +132,65 @@ void loop() {
 //////////////////////////////////////////////////////
 //////////////////BAGNO GIORNO
 /////////////////////////////////////////////////////
-  dhtbagno.temperature().getEvent(&event);
-  client.subscribe("temp_bagno");   
+  dhtbagno.temperature().getEvent(&event);  
    valore=String(event.temperature)+"°C";
-   Serial.println(valore);
+   //Serial.println(valore);
    valore.toCharArray(valore_send,5);
    client.publish("temp_bagno", valore_send);
           
-  dhtbagno.humidity().getEvent(&event);
-  client.subscribe("um_bagno");    
+  dhtbagno.humidity().getEvent(&event);   
    valore=String(event.relative_humidity);
-   Serial.println(valore);
+   //Serial.println(valore);
    valore.toCharArray(valore_send,5);
    client.publish("um_bagno", valore_send);
 //////////////////////////////////////////////////////
 //////////////////BAGNO NOTTE
 /////////////////////////////////////////////////////
-  dhtnottebagno.temperature().getEvent(&event);
-  client.subscribe("notte_temp_bagno");   
+  dhtnottebagno.temperature().getEvent(&event);   
    valore=String(event.temperature)+"°C";
-   Serial.println(valore);
+   //Serial.println(valore);
    valore.toCharArray(valore_send,5);
    client.publish("notte_temp_bagno", valore_send);
           
   dhtnottebagno.humidity().getEvent(&event);
-  client.subscribe("notte_um_bagno");    
    valore=String(event.relative_humidity);
-   Serial.println(valore);
+   //Serial.println(valore);
    valore.toCharArray(valore_send,5);
    client.publish("notte_um_bagno", valore_send);
-   //////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
 //////////////////CAMERA
 /////////////////////////////////////////////////////
-  dhtcamera.temperature().getEvent(&event);
-  client.subscribe("temp_camera");   
+  dhtcamera.temperature().getEvent(&event); 
    valore=String(event.temperature)+"°C";
-   Serial.println(valore);
+   //Serial.println(valore);
    valore.toCharArray(valore_send,5);
    client.publish("temp_camera", valore_send);
           
-  dhtcamera.humidity().getEvent(&event);
-  client.subscribe("notte_um_bagno");    
+  dhtcamera.humidity().getEvent(&event);     
    valore=String(event.relative_humidity);
-   Serial.println(valore);
+   //Serial.println(valore);
    valore.toCharArray(valore_send,5);
    client.publish("um_camera", valore_send);
-        
-  
- client.disconnect();
 
+
+
+//client.subscribe("temp_camera"); 
+//client.subscribe("notte_um_bagno"); 
+//client.subscribe("temp_bagno"); 
+client.subscribe("luce_camera");
+//client.subscribe("notte_um_bagno");
+//client.subscribe("notte_temp_bagno");
+//client.subscribe("um_bagno"); 
+
+Serial.println(digitalRead(PINRITORNOCAMERA));
+
+
+
+/////////////////////////////////////////////////////////
+////////////CONTROLLO LUCI
+////////////////////////////////////////////////////////
+
+int v = analogRead(A15);
+//Serial.println(v);
+  
 }
